8282e1d1996d91eb1978d867b06f9d395be6d66d
Jay Dhinoja <jay.dhinoja@co-co084248.ad.cabinetoffice.gov.uk>
2025-03-13 13:10:19 +0000

    code cleanup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,34 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  //   redirects: async () => {
-  //     debugger;
-  //     console.log(
-  //       "======",
-  //       `${process.env.NEXT_PUBLIC_COGNITO_AUTH_DOMAIN}/logout?client_id=${
-  //         process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID
-  //       }&logout_uri=${encodeURIComponent(
-  //         "http://localhost:3000/signed-out"
-  //       )}&response_type=code&scope=email+openid+phone&redirect_uri=${encodeURIComponent(
-  //         `${process.env.NEXTAUTH_URL}/api/auth/cognito`
-  //       )}`
-  //     );
-  //     return [
-  //       {
-  //         source: "/api/sso/logout",
-  //         destination: `${
-  //           process.env.NEXT_PUBLIC_COGNITO_AUTH_DOMAIN
-  //         }/logout?client_id=${
-  //           process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID
-  //         }&logout_uri=${encodeURIComponent(
-  //           "http://localhost:3000/signed-out"
-  //         )}&response_type=code&scope=email+openid+phone&redirect_uri=${encodeURIComponent(
-  //           `${process.env.NEXTAUTH_URL}/api/auth/cognito`
-  //         )}`,
-  //         permanent: false,
-  //       },
-  //     ];
-  //   },
 };
 
 module.exports = nextConfig;
